a5f5969e6c18e4d23c124204b691433c2ad9fdec
Japi-longlegs <mochamadrizkyka@gmail.com>
2026-07-24 22:25:19 +0700

    Update morphology_embedding.py

--- ginanita/embeddings/morphology_embedding.py
+++ ginanita/embeddings/morphology_embedding.py
@@ -3,15 +3,9 @@ import torch.nn as nn
 from ..config import GinanitaConfig
 
 class ElinaMorphologyEmbedding(nn.Module):
-    """
-    Elina: Morphology/POS Embedding.
-    Mengubah tag kelas kata (POS tag) menjadi vektor berdimensi kecil.
-    """
     def __init__(self, config: GinanitaConfig):
         super().__init__()
         self.emb = nn.Embedding(config.pos_size, config.d_pos)
-        
-        # Inisialisasi normal
         nn.init.normal_(self.emb.weight, mean=0.0, std=0.02)
 
     def forward(self, pos_ids):
