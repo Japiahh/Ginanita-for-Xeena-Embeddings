ecf95b17f7394da474683f1ee587c2b15e7e3bf6
Japi-longlegs <mochamadrizkyka@gmail.com>
2026-07-24 22:24:50 +0700

    Update dependency_embedding.py

--- ginanita/embeddings/dependency_embedding.py
+++ ginanita/embeddings/dependency_embedding.py
@@ -8,7 +8,6 @@ class LekhaDependencyEmbedding(nn.Module):
     """
     def __init__(self, config: GinanitaConfig):
         super().__init__()
-        # Relasi (root/punct/obj) -> Dimensi Kecil (d_dep)
         self.emb = nn.Embedding(config.dep_size, config.d_dep)
         
     def forward(self, x): 
